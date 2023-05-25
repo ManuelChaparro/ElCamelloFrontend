7f55bd48917d077049d61963bf761f46182c7cfd
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-05-25 01:52:05 -0500

    crearusuario

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { timer } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { RoutesListService } from '../routes-list.service';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-create-user',
@@ -22,6 +23,7 @@ export class CreateUserComponent {
   public document_type: string;
   public document: number|undefined;
   public birthdate: string;
+  public view: number;
 
   constructor(private router: Router, private http: HttpClient, private routesList: RoutesListService){
     this.name = '';
@@ -34,6 +36,7 @@ export class CreateUserComponent {
     this.document_type = 'Cédula de Ciudadanía';
     this.document = undefined;
     this.birthdate = '';
+    this.view = 0;
   }
 
   public registerUser() {
@@ -47,56 +50,32 @@ export class CreateUserComponent {
     const v_data_empty = this.validateInputs();
     if(v_email && v_pass && v_birthdate && v_data_empty){
       this.httpPostRequest();
+    }else{
+      const modal = document.querySelector('#myModal') as HTMLElement;
+      const bootstrapModal = new bootstrap.Modal(modal);
+      bootstrapModal.show();
     }
   }
 
   public validateInputs(){
-    const warn_name = document.querySelector('.warn_name') as HTMLElement;
-    const warn_surname = document.querySelector('.warn_surname') as HTMLElement;
-    const warn_phone = document.querySelector('.warn_phone') as HTMLElement;
-    const warn_doc = document.querySelector('.warn_doc') as HTMLElement;
-    if(this.name == ''){
-      warn_name.style.display = 'flex';
+    if(this.name != '' && this.surname != ''
+    && this.number != undefined && this.document != undefined){
+      return true;
     }else{
-      warn_name.style.display = 'none';
-      if(this.surname == ''){
-        warn_surname.style.display = 'flex';
-      }else{
-        warn_surname.style.display = 'none';
-        if(this.number == undefined){
-          warn_phone.style.display = 'flex';
-        }else{
-          warn_phone.style.display = 'none';
-          if(this.document == undefined){
-            warn_doc.style.display = 'flex';
-          }else{
-            warn_doc.style.display = 'none';
-            return true;
-          }
-        }
-      }
+      alert("inputs");
+      return false;
     }
-    return false;
   }
 
   public validationBirthdate(){
-    const warn_birth = document.querySelector('.warn_birth') as HTMLElement;
-    const warn_birth_more = document.querySelector('.warn_birth_more') as HTMLElement;
-    if(this.birthdate == ''){
-      warn_birth.style.display = 'flex';
-      return false;
+    const actualDate: Date = new Date();
+    actualDate.setFullYear(actualDate.getFullYear() - 16 );
+    if(this.birthdate != '' && new Date(this.birthdate).getTime() <= actualDate.getTime()){
+      return true;
     }else{
-      warn_birth.style.display = 'none';
-      const actualDate: Date = new Date();
-      actualDate.setFullYear(actualDate.getFullYear() - 16 );
-      if(new Date(this.birthdate).getTime() <= actualDate.getTime()){
-        warn_birth_more.style.display = 'none';
-        return true;
-      }else{
-        warn_birth_more.style.display = 'flex';
-      }
+      alert("cumple");
+      return false;
     }
-    return false;
   }
 
   public httpPostRequest(){
@@ -122,13 +101,9 @@ export class CreateUserComponent {
       data.tipo_documento = 'C.E';
     }
     this.http.post(url, data).subscribe(response => {
-
       if('message' in response){
         if(response.message == '0'){
-          const container = document.querySelector('.container') as HTMLElement;
-          const noticeAccount = document.querySelector('.noticeAccount') as HTMLElement;
-          container.style.display = 'none';
-          noticeAccount.style.display = 'flex';
+          this.view = 1;
           timer(2500).pipe(delay(2500)).subscribe(() => {
             this.router.navigate(['/login']);
           });
@@ -136,39 +111,28 @@ export class CreateUserComponent {
           alert("El email ya se encuentra registrado");
         }else{
           console.log(response);
-
         }
       }
     });
   }
 
   public validationEmail(){
-    const warn_email = document.querySelector('.warn_email') as HTMLElement;
     const email_test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if(email_test.test(this.email)){
-      warn_email.style.display = 'none';
       return true;
     }else{
-      warn_email.style.display = 'flex';
+      alert("Email");
       return false;
     }
   }
 
   public validationPass(){
-    const warn_pass = document.querySelector('.warn_pass') as HTMLElement;
-    const warn_con_pass = document.querySelector('.warn_con_pass') as HTMLElement;
-    if(this.password.length < 8){
-      warn_pass.style.display = 'flex';
+    if(this.password.length >= 8 && this.password == this.confirm_pass){
+      return true;
     }else{
-      warn_pass.style.display = 'none';
-      if(this.password != this.confirm_pass){
-        warn_con_pass.style.display = 'flex';
-      }else{
-        warn_con_pass.style.display = 'none';
-        return true;
-      }
+      alert("Contraseña");
+      return false;
     }
-    return false;
   }
 
   changeGender(value:string): void {
@@ -183,60 +147,5 @@ export class CreateUserComponent {
     this.registerUser();
   }
 
-  onOptionId(){
-    const divIdentification = document.querySelector('#div-identification') as HTMLElement;
-    const divCredential = document.querySelector('#div-credential') as HTMLElement;
-    const divPersonalInfo = document.querySelector('#div-personalinfo') as HTMLElement;
-    const divDate = document.querySelector('#div-date') as HTMLElement;
-
-    divIdentification.style.display = 'flex';
-    divCredential.style.display = 'none';
-    divPersonalInfo.style.display = 'none';
-    divDate.style.display = 'none';
-  }
-
-  onOptionCr(){
-    const divIdentification = document.querySelector('#div-identification') as HTMLElement;
-    const divCredential = document.querySelector('#div-credential') as HTMLElement;
-    const divPersonalInfo = document.querySelector('#div-personalinfo') as HTMLElement;
-    const divDate = document.querySelector('#div-date') as HTMLElement;
-    divIdentification.style.display = 'none';
-    divCredential.style.display = 'flex';
-    divPersonalInfo.style.display = 'none';
-    divDate.style.display = 'none';
-  }
-
-  onOptionPi(){
-    const divIdentification = document.querySelector('#div-identification') as HTMLElement;
-    const divCredential = document.querySelector('#div-credential') as HTMLElement;
-    const divPersonalInfo = document.querySelector('#div-personalinfo') as HTMLElement;
-    const divDate = document.querySelector('#div-date') as HTMLElement;
-    divIdentification.style.display = 'none';
-    divCredential.style.display = 'none';
-    divPersonalInfo.style.display = 'flex';
-    divDate.style.display = 'flex';
-  }
-
 }
 
-window.addEventListener("resize", validateScreenSize);
-
-function validateScreenSize() {
-  const divIdentification = document.querySelector('#div-identification') as HTMLElement;
-  const divCredential = document.querySelector('#div-credential') as HTMLElement;
-  const divPersonalInfo = document.querySelector('#div-personalinfo') as HTMLElement;
-  const divDate = document.querySelector('#div-date') as HTMLElement;
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
-  if(screenWidth>= 800){
-    divIdentification.style.display = 'flex';
-    divCredential.style.display = 'flex';
-    divPersonalInfo.style.display = 'flex';
-    divDate.style.display = 'flex';
-  }else{
-    divIdentification.style.display = 'flex';
-    divCredential.style.display = 'none';
-    divPersonalInfo.style.display = 'none';
-    divDate.style.display = 'none';
-  }
-}
