3fe15351c054ed9806e33635401d10ff114d8138
ManuelChaparro <manuel.chaparro01@uptc.edu.co>
2023-05-27 03:29:07 -0500

    recover fixed

--- src/app/recover-pass/recover-pass.component.ts
+++ src/app/recover-pass/recover-pass.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { timer } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { RoutesListService } from '../routes-list.service';
 
 @Component({
   selector: 'app-recover-pass',
@@ -6,5 +11,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./recover-pass.component.css']
 })
 export class RecoverPassComponent {
+  public email: string;
+  public document: number | undefined;
+
+  constructor(private http: HttpClient, private router: Router, private routesList: RoutesListService){
+    this.email = '';
+    this.document = undefined;
+  }
+
+  public sendRecovery(){
+    if(this.validations()){
+      const url = this.routesList.getRecoverPass();
+      const data = {
+        email: this.email,
+        identificacion: this.document
+      };
+      this.http.post(url, data).subscribe(response => {
+        if('message' in response){
+          if(response.message == '0'){
+            const div_info = document.querySelector('#div_info') as HTMLElement;
+            const notice_recovery = document.querySelector('#notice_recovery') as HTMLElement;
+            div_info.classList.add('d-none');
+            notice_recovery.classList.remove('d-none');
+            timer(1500).pipe(delay(1500)).subscribe(() => {
+              const routing = document.querySelector('.routing') as HTMLElement;
+              routing.innerHTML = '3';
+              timer(0).pipe(delay(900)).subscribe(() => {
+                routing.innerHTML = '2';
+                timer(0).pipe(delay(900)).subscribe(() => {
+                  routing.innerHTML = '1';
+                  timer(0).pipe(delay(900)).subscribe(() => {
+                    this.router.navigate(['/login']);
+                  });
+                });
+              });
+            });
+          }else{
+            console.log(response);
+          }
+        }
+      });
+    }
+  }
+
+  animationEmail(){
+    const icon = document.querySelector('#email-icon') as HTMLElement;
+    icon.classList.add('fa-spin');
+  }
+
+  animationDoc(){
+    const icon = document.querySelector('#doc-icon') as HTMLElement;
+    icon.classList.add('fa-spin');
+  }
+
+  public validations(){
+    const warn_email = document.querySelector('.warn_email') as HTMLElement;
+    const email_test = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(email_test.test(this.email)){
+      warn_email.style.display = 'none';
+      return true;
+    }else{
+      warn_email.style.display = 'flex';
+      return false;
+    }
+  }
 
 }
